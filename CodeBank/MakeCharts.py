4f757ae1453521c7e4628eb32ba49a439134f12e
Michael Wegner <miwe7500@colorado.edu>
2019-04-28 21:27:15 -0600

    Created neww visulizations for portfolio value and returns

--- CodeBank/MakeCharts.py
+++ CodeBank/MakeCharts.py
@@ -7,9 +7,10 @@ import pandas as pd
 
 
 
-file_name="121217-127617.csv"
-file_path="../Data/Test/"+file_name
+file_name="071818-092718.csv"
+file_path="../Data/Test/Noise3.0/"+file_name
 Returns_in = pd.read_csv(file_path,delimiter=" ")
+print(len(Returns_in))
 
 ob_ret=(Returns_in["Returns"])
 
@@ -18,20 +19,19 @@ PortValue=[]
 
 PortValue.append(100000)
 
-for x in range(0,9):
-	print(x)
+for x in range(0,50):
 	PortValue.append(PortValue[x]*(1+ob_ret[x]))
 
 # Data for plotting
-t = np.arange(0, 10, 1)
+t = np.arange(0, 51, 1)
 ##Change to returns to get returns vs time
-s = ob_ret
+s = PortValue
 
 fig, ax = plt.subplots()
 ax.plot(t, s)
 
-ax.set(xlabel='time (days)', ylabel='Returns',
-       title='Returns VS Time')
+ax.set(xlabel='time (days)', ylabel='Portfolio Value',
+       title='Portfolio Value VS Time')
 ax.grid()
 
-fig.savefig("Returnsover10days.png")
+fig.savefig("Noise3_PortValue_vs_time_50.png")
